6b18e15bd7c616266ad37713b1b55f929284ce97
Vivek Kumar Singh <vschs007@gmail.com>
2016-11-25 16:33:13 +0530

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#
 # Bootstrap the Carthage dependencies. If the Carthage directory
 # already exists then nothing is done. This speeds up builds on
 # CI services where the Carthage directory can be cached.
@@ -19,7 +17,6 @@ fi
 if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
   rm -rf Carthage/*
 fi
-
 # Only enable this on the Xcode Server because it times out if it does not
 # get any output for some time while building the dependencies.
 
@@ -27,7 +24,6 @@ CARTHAGE_VERBOSE=""
 if [ ! -z "$XCS_BOT_ID"  ]; then
   CARTHAGE_VERBOSE="--verbose"
 fi
-
 if [ ! -f Carthage/Cartfile.resolved ]; then
   carthage bootstrap $CARTHAGE_VERBOSE --platform ios --color auto --no-use-binaries
   cp Cartfile.resolved Carthage/Cartfile.resolved
