26705218931538b75e846fc17eae40fa70f7d45a
Vivek Kumar Singh <vschs007@gmail.com>
2017-03-20 16:20:32 +0530

    spacing
    spacing

--- l10n-screenshots.sh
+++ l10n-screenshots.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ -d l10n-screenshots ]; then
   echo "The l10n-screenshots directory already exists. You decide."
-  exit 1
+    exit 1
 fi
 if [ ! -d firefox-ios-l10n ]; then
     echo "Did not find a firefox-ios-l10n checkout. Are you running this on a localized build?"
-    exit 1
-    fi
+     exit 1
+     fi
 mkdir l10n-screenshots
 for d in firefox-ios-l10n/?? firefox-ios-l10n/??? firefox-ios-l10n/??-??; do
     lang=$(basename $d)
